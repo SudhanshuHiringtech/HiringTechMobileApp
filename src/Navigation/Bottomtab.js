91b3f45d867b584ff8eed08dbb5e08c5f42b8c0c
Sam <csumit961@gmail.com>
2024-07-24 12:45:40 +0530

    slight ui fixes for bottom nav

--- src/Navigation/Bottomtab.js
+++ src/Navigation/Bottomtab.js
@@ -1,126 +1,150 @@
-import * as React from 'react';
-import { View, Text,Image,StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import Home from "../Screen/BottomTabScreen/Home";
+import Toptab from "./Toptab";
+import Appleid from "../Screen/BottomTabScreen/Applied";
 
-import Home from '../Screen/BottomTabScreen/Home';
-import Toptab from './Toptab';
-import Appleid from "../Screen/BottomTabScreen/Applied"
-
-import Message from '../Screen/BottomTabScreen/Message';
+import Message from "../Screen/BottomTabScreen/Message";
 const Tab = createBottomTabNavigator();
 
+const Bottomtab = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: styles.tabBarStyle,
+                tabBarItemStyle: styles.tabBarItemStyle,
+            }}
+            initialRouteName="Home">
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require("../Assets/home.png")}
+                                style={{
+                                    ...styles.imageStyle,
+                                    tintColor: focused ? "#D79442" : "#175574",
+                                }}
+                            />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused, color }) => (
+                        <Text
+                            style={[
+                                styles.text,
+                                { fontWeight: focused ? "bold" : "normal" },
+                            ]}>
+                            Dashboard
+                        </Text>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Job"
+                component={Toptab}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require("../Assets/jobs.png")}
+                                style={{
+                                    ...styles.imageStyle,
+                                    tintColor: focused ? "#D79442" : "#175574",
+                                }}
+                            />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused, color }) => (
+                        <Text
+                            style={[
+                                styles.text,
+                                { fontWeight: focused ? "bold" : "normal" },
+                            ]}>
+                            Jobs
+                        </Text>
+                    ),
+                }}
+            />
 
-const Bottomtab = ()=> {
-  return (
-    <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-    <Tab.Screen name="Home" component={Home} 
-    options={{
-        tabBarIcon: ({focused}) => (
-          <View>
-            <Image
-              source={require('../Assets/home.png')}
-              style={{
-                ...styles.imageStyle,
-                tintColor: focused ? '#81402f' : '#175574',
-              }}
+            <Tab.Screen
+                name="Appleid"
+                component={Appleid}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require("../Assets/Applied.png")}
+                                style={{
+                                    ...styles.imageStyle,
+                                    tintColor: focused ? "#D79442" : "#175574",
+                                }}
+                            />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused, color }) => (
+                        <Text
+                            style={[
+                                styles.text,
+                                { fontWeight: focused ? "bold" : "normal" },
+                            ]}>
+                            Applied
+                        </Text>
+                    ),
+                }}
             />
-          </View>
-        ),
-        tabBarLabel: ({focused, color}) => (
-          <Text
-            style={{color: focused ? '#81402f' : '#175574'}}>
-            Dashboard
-          </Text>
-        ),
-      }}
-    />
-    <Tab.Screen name="Job" component={Toptab}
-    options={{
-        tabBarIcon: ({focused}) => (
-          <View>
-            <Image
-              source={require('../Assets/jobs.png')}
-              style={{
-                ...styles.imageStyle,
-                tintColor: focused ? '#81402f' : '#175574',
-              }}
-            />
-          </View>
-        ),
-        tabBarLabel: ({focused, color}) => (
-          <Text
-            style={{color: focused ? '#81402f' : '#175574'}}>
-            jobs
-          </Text>
-        ),
-      }}
-    />
 
-    <Tab.Screen name="Appleid" component={Appleid} 
-     options={{
-        tabBarIcon: ({focused}) => (
-          <View>
-            <Image
-              source={require('../Assets/Applied.png')}
-              style={{
-                ...styles.imageStyle,
-                tintColor: focused ? '#81402f' : '#175574',
-              }}
+            <Tab.Screen
+                name="Message"
+                component={Message}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View>
+                            <Image
+                                source={require("../Assets/message.png")}
+                                style={{
+                                    ...styles.imageStyle,
+                                    tintColor: focused ? "#D79442" : "#175574",
+                                }}
+                            />
+                        </View>
+                    ),
+                    tabBarLabel: ({ focused, color }) => (
+                        <Text
+                            style={[
+                                styles.text,
+                                { fontWeight: focused ? "bold" : "normal" },
+                            ]}>
+                            Messages
+                        </Text>
+                    ),
+                }}
             />
-          </View>
-        ),
-        tabBarLabel: ({focused, color}) => (
-          <Text
-            style={{color: focused ? '#81402f' : '#175574'}}>
-            Applied
-          </Text>
-        ),
-      }}
-    />
-  
-    <Tab.Screen name="Message" component={Message} 
-    options={{
-        tabBarIcon: ({focused}) => (
-          <View>
-            <Image
-              source={require('../Assets/message.png')}
-              style={{
-                ...styles.imageStyle,
-                tintColor: focused ? '#81402f' : '#175574',
-              }}
-            />
-          </View>
-        ),
-        tabBarLabel: ({focused, color}) => (
-          <Text
-            style={{color: focused ? '#81402f' : '#175574'}}>
-            Messages
-          </Text>
-        ),
-      }}
-    />
-  </Tab.Navigator>
-  );
-}
+        </Tab.Navigator>
+    );
+};
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      //backgroundColor:'#fff'
-    },
-    tabBarStyle: {
-      backgroundColor: '#FDD312',
-      height: 60,
-      borderTopRightRadius: 50,
-      borderTopLeftRadius: 50,
-      paddingHorizontal:20,
+    text: {
+        color: "#175574",
+        fontSize: 12,
     },
     imageStyle: {
-      width: 24,
-      height: 24,
-      resizeMode: 'contain',
+        width: 24,
+        height: 24,
+        resizeMode: "contain",
     },
-  });
+    tabBarStyle: {
+        paddingHorizontal: 20,
+        height: 65,
+    },
+    tabBarItemStyle: {
+        resizeMode: "contain",
+        marginBottom: 10,
+    },
+});
 export default Bottomtab;
